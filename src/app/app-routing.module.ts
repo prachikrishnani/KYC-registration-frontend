0e3e4b1588c07bf633b497bf909bfa7472b9b388
Prachi <krishnaniprachi123@gmail.com>
2024-02-04 23:23:39 +0530

    project setup done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KycComponent } from './kyc/kyc.component';
+import { VideoComponent } from './kyc/video/video.component';
+import { OtpComponent } from './kyc/otp/otp.component';
+import { PasswordComponent } from './kyc/password/password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: KycComponent, children: [
+      { path: '', redirectTo: 'video', pathMatch: 'full' },
+      { path: 'video', component: VideoComponent },
+      { path: 'otp', component: OtpComponent },
+      { path: 'password', component: PasswordComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KycComponent } from './kyc/kyc.component';
+import { VideoComponent } from './kyc/video/video.component';
+import { OtpComponent } from './kyc/otp/otp.component';
+import { PasswordComponent } from './kyc/password/password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KycComponent,
+    VideoComponent,
+    OtpComponent,
+    PasswordComponent
   ],
   imports: [
     BrowserModule,
